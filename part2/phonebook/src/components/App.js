881e6fcaad904469042c020861caf3bac8f2ef3f
Ivan Lytovka <ivanlytovka@gmail.com>
2019-06-20 00:17:24 +0300

    Add data fetching from local db

--- part2/phonebook/src/components/App.js
+++ part2/phonebook/src/components/App.js
@@ -1,21 +1,27 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
 import Filter from './Filter'
 import PersonsForm from './PersonsForm'
 import Person from './Person'
 import Persons from './Persons'
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-123456' },
-        { name: 'Ada Lovelace', number: '39-44-5323523' },
-        { name: 'Dan Abramov', number: '12-43-234345' },
-        { name: 'Mary Poppendieck', number: '39-23-6423122' }
-    ]);
+    const [persons, setPersons] = useState([]);
     const [newName, setNewName] = useState('');
     const [newNumber, setNewNumber] = useState('');
     const [filterName, setFilterName] = useState('');
 
     const [showAllNames, setShowAllNames] = useState(true);
+
+    useEffect(() => {
+        axios.get("http://localhost:3001/persons")
+        .then((response) => {
+            setPersons(response.data);
+        })
+    }, []);
+
+    // console.log('prossessed', persons.length, 'names');
 
     const namesToDisplay = showAllNames ? persons : persons.filter((person) => {
         return person.name.toLocaleLowerCase().includes(filterName.toLocaleLowerCase());
